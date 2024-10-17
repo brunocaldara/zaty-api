46ef2803be173c8c881782936f4615e70915cfd0
Bruno Caldara <brunocaldara@gmail.com>
2024-10-16 21:53:07 -0300

    Altera a classe de conexao

--- core/database.py
+++ core/database.py
@@ -25,3 +25,39 @@ async def get_session() -> AsyncGenerator:
         yield session
     finally:
         await session.close()
+
+
+# class DbConnection():
+#     def __init__(self) -> None:
+#         self.__DB_URL = 'postgresql+asyncpg://postgres:PWMxXt7uiR9ALeMcXigd@95.216.214.177:5432/zaty'
+#         self.__engine = self.__create_database_engine()
+#         self.session = None
+
+#     def __create_database_engine(self):
+#         engine: AsyncEngine = create_async_engine(
+#             self.__DB_URL
+#         )
+#         return engine
+
+#     def get_engine(self):
+#         return self.__engine
+
+#     def __enter__(self):
+#         print('enter')
+#         Session: AsyncSession = sessionmaker(
+#             expire_on_commit=False,
+#             autoflush=False,
+#             class_=AsyncSession,
+#             bind=self.__engine
+#         )
+#         self.session = Session()
+#         return self
+
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         print('exit')
+#         self.session.close()
+
+
+# if __name__ == '__main__':
+#     with DbConnection() as db:
+#         print('Criou')
