24beca07ef33425722cd1e2229cbcd48d299979c
Bruno Caldara <brunocaldara@gmail.com>
2024-11-03 19:54:39 -0300

    Adiciona funções para customizar interface do Swagger

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import os
 import aiohttp
 from dotenv import load_dotenv
 from fastapi import Body, FastAPI
+from fastapi.openapi.docs import (get_swagger_ui_html,
+                                  get_swagger_ui_oauth2_redirect_html)
+from fastapi.openapi.utils import get_openapi
 
 from src.api.v1 import api_routes
 from src.core.settings import settings
@@ -30,10 +33,44 @@ app = FastAPI(title='Zaty API',
               description='API destinada ao projeto Zaty',
               )
 
+
+# @app.get("/docs", include_in_schema=False)
+# async def custom_swagger_ui_html():
+#     # return get_swagger_ui_html(
+#     #     openapi_url=app.openapi_url,
+#     #     title=app.title,
+#     #     oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+#     #     swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js",
+#     #     swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css")
+
+#     return get_swagger_ui_html(
+#         openapi_url=app.openapi_url,
+#         title=f"{app.title} - Swagger UIAAAAA",
+#         swagger_css_url="https://cdn.jsdelivr.net/gh/Itz-fork/Fastapi-Swagger-UI-Dark/assets/swagger_ui_dark.min.css"
+#     )
+
+# def custom_openapi():
+#     if app.openapi_schema:
+#         return app.openapi_schema
+#     openapi_schema = get_openapi(
+#         title="Custom title",
+#         version="2.5.0",
+#         description="This is a very custom OpenAPI schema",
+#         routes=app.routes,
+#     )
+#     openapi_schema["info"]["x-logo"] = {
+#         "url": "https://www.google.com/imgres?q=php&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F2%2F27%2FPHP-logo.svg&imgrefurl=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FPHP&docid=NtM0wDWraxtdoM&tbnid=QRXpr0h-i79qeM&vet=12ahUKEwj0rYrz8cCJAxWJO7kGHW9vA5wQM3oECBsQAA..i&w=711&h=384&hcb=2&ved=2ahUKEwj0rYrz8cCJAxWJO7kGHW9vA5wQM3oECBsQAA"
+#     }
+#     app.openapi_schema = openapi_schema
+#     return app.openapi_schema
+
+
+# app.openapi = custom_openapi
+
 app.include_router(api_routes.router, prefix=settings.API_URL_VERISON)
 
 
-@app.get('/')
+@ app.get('/')
 async def index():
     return {'msg': 'Zaty API funcionando!'}
 
